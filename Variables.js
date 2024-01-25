6e997104675225be8dca1658fa3b88fbe05e32f5
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-01-26 01:54:34 +0600

    Variables

---
+++ Variables.js
@@ -0,0 +1,17 @@
+
+// Variable ==> vary + able 
+// 5 things to declear a variable
+
+// Valid variables
+
+var weight = 38;
+var price = 590;
+var year = 2024;
+var penItems = 30;
+
+// Not valid variables
+
+// var age;
+// console.log(age);
+
+console.log(price);
