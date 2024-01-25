ad23b62adb77e6554fce10ecbe208c8c32d1d7f0
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-01-26 02:25:52 +0600

    Updated

---
+++ DataTypes.js
@@ -0,0 +1,16 @@
+// 16-4 Variable Data Types, Numeric, String, Boolean
+
+
+// Number ==> var = 40;
+// String ==> var = "Pappu";
+// Boolean ==> var Pasent = true;
+
+// Not valid pappu is not a number it's name so need to ""
+
+// var name = pappu;
+// console.log(name)
+
+// Valid 
+var name = "Pappu";
+console.log(name)
+
